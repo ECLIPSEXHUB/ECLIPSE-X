5e1114577252d912b37d28e903d352179b916dfb
ECLIPSEXHUB <90097185+ECLIPSEXHUB@users.noreply.github.com>
2022-07-23 19:33:19 +0200

    Update MadCity.lua

--- ZEPHYR REPO/MadCity.lua
+++ ZEPHYR REPO/MadCity.lua
@@ -1,1 +1,5 @@
 print("MAD CITY IS DOWN!")
+    local notiflib = loadstring(game:HttpGet("https://gist.githubusercontent.com/ECLIPSEXHUB/2eb5faec47ed518ae47914744b6c1251/raw/", true))()
+    notiflib:NewNotif([[MAD CITY IS BROKEN! 
+we try out best to fix it
+    ]], 3)
